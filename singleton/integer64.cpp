395331f7da4b15829124a4e45dc1f863eab32e97
Antoine bureau <bureauAntoine@cb4tech.com>
2024-05-08 12:24:46 +0200

    add uinteger singleton

--- singleton/integer64.cpp
+++ singleton/integer64.cpp
@@ -49,3 +49,49 @@ QDateTime Integer64::toDate()
     dt.setMSecsSinceEpoch(valueInt);
     return dt;
 }
+
+
+UInteger64::UInteger64(QObject *parent)
+    : QObject(parent)
+{}
+
+void UInteger64::registerQml()
+{
+    qmlRegisterType<Integer64>("UInteger64", 1, 0, "UInteger64");
+}
+
+QString UInteger64::value() const
+{
+    return m_value;
+}
+
+void UInteger64::setValue(QString value)
+{
+    if (m_value == value)
+        return;
+
+    m_value = value;
+    emit valueChanged(m_value);
+}
+
+QString UInteger64::toString()
+{
+    while (m_value.length() < 8)
+    {
+        m_value.append(0);
+    }
+    uint64_t valueInt = *((uint64_t*)m_value.toStdString().c_str());
+    return QString::number(valueInt);
+}
+
+QDateTime UInteger64::toDate()
+{
+    while (m_value.length() < 8)
+    {
+        m_value.append(0);
+    }
+    uint64_t valueInt = *((uint64_t*)m_value.toStdString().c_str());
+    QDateTime dt;
+    dt.setMSecsSinceEpoch(valueInt);
+    return dt;
+}
--- singleton/integer64.h
+++ singleton/integer64.h
@@ -29,4 +29,28 @@ private:
     QString m_value;
 };
 
+class UInteger64 : public QObject
+{
+    Q_OBJECT
+public:
+    explicit UInteger64(QObject *parent = nullptr);
+    static void registerQml();
+    Q_PROPERTY(QString value READ value WRITE setValue NOTIFY valueChanged)
+    QString value() const;
+    void setValue(QString value);
+
+    Q_INVOKABLE QString toString();
+    Q_INVOKABLE QDateTime toDate();
+
+public slots:
+
+signals:
+    void valueChanged(QString value);
+
+private slots:
+
+private:
+    QString m_value;
+};
+
 #endif // INTEGER64_H
